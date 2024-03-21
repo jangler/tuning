ec2268ad37e5257ba9c577ad95c2d158fd0e5123
Casey Mulcahy <caseymulcahy@proton.me>
2024-03-21 08:43:53 -0400

    Add 61-key Exquis heatmap

--- scripts/layout_heatmap.py
+++ scripts/layout_heatmap.py
@@ -1,5 +1,5 @@
 DESCRIPTION = """
-Plot a heatmap of good step tunings for 39-key Exquis layouts.
+Plot a heatmap of good step tunings for isomorphic note layouts.
 
 Layouts are scored by the sum of the inverse Tenney height of intervals
 within an integer limit approximated within an error limit, taking the
@@ -7,25 +7,60 @@ center pad as 1/1. Point values for each interval are scaled based on
 the square of the absolute error of the best approximation, down to 0
 at the error limit.
 
-Computation will take a few minutes.
+Computation may take a few minutes.
 """
 
 from argparse import ArgumentParser
+from dataclasses import dataclass
 import shelve
 from fractions import Fraction
 from itertools import chain
 from math import log, log2
-from multiprocessing import Pool
-from typing import Iterable
+from typing import Callable, Iterable
+
+def exquis_vectors() -> list[tuple[int, int]]:
+    def row(g1: int, g2: int) -> Iterable[tuple[int, int]]:
+        length = 5 if (g1 + g2) % 2 == 0 else 6
+        return ((g1 + 1 - i, g2 - 1 + i) for i in range(length))
+    
+    return list(chain(
+        row(4, 1), row(3, 1), row(3, 0), row(2, 0), row(2, -1),
+        row(1, -1),
+        row(1, -2), row(0, -2), row(0, -3), row(-1, -3), row(-1, -4)
+    ))
+
+def exquis39_vectors() -> list[tuple[int, int]]:
+    def row(g1: int, g2: int) -> Iterable[tuple[int, int]]:
+        length = 3 if (g1 + g2) % 2 == 0 else 4
+        return ((g1 - i, g2 + i) for i in range(length))
+    
+    return list(chain(
+        row(4, 1), row(3, 1), row(3, 0), row(2, 0), row(2, -1),
+        row(1, -1),
+        row(1, -2), row(0, -2), row(0, -3), row(-1, -3), row(-1, -4)
+    ))
+
+@dataclass
+class Controller:
+    name: str
+    x_step_name: str
+    y_step_name: str
+    vector_func: Callable
+
+controllers = {
+    'exquis': Controller('Exquis', 'Up-right', 'Up-left', exquis_vectors),
+    'exquis39': Controller('Exquis 39-key', 'Up-right', 'Up-left', exquis39_vectors),
+}
 
 parser = ArgumentParser(description=DESCRIPTION)
 parser.add_argument('--error-limit', type=float, default=15)
 parser.add_argument('--integer-limit', type=int, default=16)
 parser.add_argument('--cached', action='store_true')
+parser.add_argument('controller', nargs='+', choices=controllers.keys())
 args = parser.parse_args()
 
 import plotly.express as px
-from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 def integer_limit_intervals(limit: int) -> set[Fraction]:
     return set(Fraction(n, d)
@@ -41,25 +76,19 @@ def tenney_height(r: Fraction) -> float:
 step_range = list(range(args.error_limit, 702 + args.error_limit))
 error_limit_squared = args.error_limit ** 2
 
-# Discard 1/1 since its inverse Tenney height is undefined.
+# Since we're measuring from the center of the keyboard, intervals are
+# symmetric with their inverses. So we can ignore all intervals < 1,
+# and then just double the score to get the same result. We also ignore
+# 1/1 since its inverse Tenney height is undefined.
 rs = [r for r in integer_limit_intervals(args.integer_limit)
-      if r != Fraction(1)]
+      if r > 1]
 
 interval_cents = {r: cents(r) for r in rs}
 
 def point_value(interval: Fraction, cents: float, best_approximation: float) -> float:
     error = abs(best_approximation - cents)
-    return 1 / tenney_height(interval) * max(0, 1 - error*error / error_limit_squared)
-
-def row(g1: int, g2: int) -> Iterable[tuple[int, int]]:
-    length = 3 if (g1 + g2) % 2 == 0 else 4
-    return ((g1 - i, g2 + i) for i in range(length))
-
-vectors = list(chain(
-    row(4, 1), row(3, 1), row(3, 0), row(2, 0), row(2, -1),
-    row(1, -1),
-    row(1, -2), row(0, -2), row(0, -3), row(-1, -3), row(-1, -4)
-))
+    # Divide 2 instead of 1 here to make up for ignoring intervals < 1.
+    return 2 / tenney_height(interval) * max(0, 1 - error*error / error_limit_squared)
 
 cache = {}
 cache_path = 'scripts/layout_heatmap'
@@ -67,37 +96,57 @@ cache_path = 'scripts/layout_heatmap'
 def closest(x: float, xs: list[float]) -> float:
     return min(xs, key=lambda y: abs(x - y))
 
-def layout_score(steps: tuple[float, float]) -> float:
+def write_figure(controller: str, matrix: list[list[float]]):
+    # Round the scores to reduce file size.
+    matrix = [[round(x, 1) for x in row] for row in matrix]
+    c = controllers[controller]
+    title = f'LCJI approximation by {c.name} note layouts'
+    labels = {
+        'x': f'{c.x_step_name} step',
+        'y': f'{c.y_step_name} step',
+        'color': 'Score',
+    }
+    fig = px.imshow(matrix,
+                    title=title,
+                    labels=labels,
+                    x=step_range,
+                    y=step_range)
+    # Use a fixed div_id to avoid VCS noise from UUID regeneration.
+    fig.write_html(f'src/layout-heatmaps/{controller}.html',
+                   include_plotlyjs='cdn',
+                   div_id='figure')
+
+def layout_score(vectors: list[tuple[int, int]], steps: tuple[float, float]) -> float:
     key = tuple(sorted(steps))
     if key not in cache:
         cents_values = [steps[0]*v[0] + steps[1]*v[1] for v in vectors]
         cache[key] = sum(point_value(r, c, closest(c, cents_values))
-                         for r, c in interval_cents.items())
+                        for r, c in interval_cents.items())
     return cache[key]
 
-def write_figure(matrix: list[list[float]]):
-    # Round the scores to reduce file sizes.
-    matrix = [[round(x, 1) for x in row] for row in matrix]
-    fig = px.imshow(matrix,
-                    title='LCJI approximation by Exquis 39-key note layouts',
-                    labels={'x': 'Up-right step', 'y': 'Up-left step', 'color': 'Score'},
-                    x=step_range,
-                    y=step_range)
-    fig.write_html('src/layout-heatmaps/exquis39.html', include_plotlyjs='cdn')
-    with shelve.open(cache_path) as db:
-        db['exquis39'] = matrix
+def score_row(args: tuple) -> list[float]:
+    vectors, upleft = args
+    return [layout_score(vectors, (upleft, upright)) for upright in step_range]
 
-def main():
+def generate(controller: str):
+    print(f'Generating {controller} plot')
+    vectors = controllers[controller].vector_func()
+
     if args.cached:
         with shelve.open(cache_path) as db:
-            matrix = db['exquis39']
+            matrix = db[controller]
     else:
-        with Pool(4) as p:
-            matrix = [p.map(layout_score, [(upleft, upright)
-                                           for upright in step_range])
-                      for upleft in tqdm(step_range)]
+        matrix = process_map(score_row, [(vectors, upleft)
+                                         for upleft in step_range])
 
-    write_figure(matrix)
+    write_figure(controller, matrix)
+    
+    with shelve.open(cache_path) as db:
+        db[controller] = matrix
+
+def main():
+    for controller in args.controller:
+        generate(controller)
 
 if __name__ == '__main__':
     main()
